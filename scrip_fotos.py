57060014f54130981a04a2120c69bbc006b7138b
albertocastrovill <josealberto.castro@udem.edu>
2024-04-04 21:37:03 -0600

    new images chess

--- scrip_fotos.py
+++ scrip_fotos.py
@@ -13,8 +13,9 @@ if not cap.isOpened():
     print("No se puede abrir la cámara")
     exit()
 
+i=10
 # Tomar 60 fotos, una cada 2 segundos
-for i in range(10):
+for i in range(40):
     # Capturar un frame
     ret, frame = cap.read()
 
@@ -26,7 +27,7 @@ for i in range(10):
     cv2.imshow('Camera', frame)
 
     # Esperar un poco antes de la próxima captura para que se pueda ver en pantalla
-    key = cv2.waitKey(1000)  # Espera 1000 ms
+    key = cv2.waitKey(2000)  # Espera 1000 ms
 
     # Guardar el frame como imagen
     filename = os.path.join(folder_name, f'foto_{i+1}.png')
@@ -34,7 +35,7 @@ for i in range(10):
     print(f'Imagen {i+1} guardada en {filename}')
 
     # Esperar el tiempo restante para completar los 2 segundos
-    time.sleep(1.5)  # Ya esperamos 0.5 segundos con cv2.waitKey
+    time.sleep(2)  # Ya esperamos 0.5 segundos con cv2.waitKey
 
     if key & 0xFF == ord('q'):  # Permitir salir con 'q'
         break
